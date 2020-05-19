54ca49d4e473b7f3a590137e291e49d859f8a3ac
Logician724 <king2sairos2013@gmail.com>
2020-05-19 06:02:48 +0200

    Updates

---
+++ precache-manifest.e03028c4ccc4f515cd55c09d1b20a043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8392cb899101a3b12dda93db292bce4",
+    "url": "/vp-http-client/index.html"
+  },
+  {
+    "revision": "e5bde19198e1e4d7a10f",
+    "url": "/vp-http-client/static/css/main.35958834.chunk.css"
+  },
+  {
+    "revision": "5b925541c5ec7cc3629e",
+    "url": "/vp-http-client/static/js/2.884ea445.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vp-http-client/static/js/2.884ea445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5bde19198e1e4d7a10f",
+    "url": "/vp-http-client/static/js/main.dad4b416.chunk.js"
+  },
+  {
+    "revision": "10f2313a933822b77d5b",
+    "url": "/vp-http-client/static/js/runtime-main.86b34cf8.js"
+  }
+]);
